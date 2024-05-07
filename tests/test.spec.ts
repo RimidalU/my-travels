44eb3b9b3aed83e1a6d22ce125777dd4f9a723c5
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-08 00:49:00 +0300

    test: change test link

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('MY-TEST', async ({ page }) => {
   await page.locator("body").click()
-  await page.goto('/')
+  await page.goto('https://my-travels-iota.vercel.app/')
   const page1Promise = page.waitForEvent('popup')
   await page.getByRole('link', { name: 'Docs12 -> Find in-depth' }).click()
   const page1 = await page1Promise
