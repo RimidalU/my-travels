9bce50bcdf9fb36e8edf27a9d01e29bf78c43739
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-09 01:13:23 +0300

    ci: refactor playwright tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,18 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+test('test Docs', async ({ page }) => {
+  await page.goto('https://my-travels-iota.vercel.app/')
+  const page1Promise = page.waitForEvent('popup')
+  await page.getByRole('link', { name: 'Docs12 -> Find in-depth' }).click()
+  const page1 = await page1Promise
+  await expect(page1.getByText('Next.js is a React framework')).toBeVisible()
 })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click()
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole('heading', { name: 'Installation' }),
-  ).toBeVisible()
+test('test  Templates', async ({ page }) => {
+  await page.goto('https://test12-indol.vercel.app/')
+  const page1Promise = page.waitForEvent('popup')
+  await page.getByRole('link', { name: 'Templates -> Explore starter' }).click()
+  const page1 = await page1Promise
+  await page1.goto('https://vercel.com/templates?framework=next.js')
+  await expect(page1.getByText('Find your Template')).toBeVisible()
 })
