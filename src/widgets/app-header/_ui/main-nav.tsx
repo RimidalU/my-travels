c6f4aae03a46cebcf005810c69a27ca6985cf579
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-12 13:07:54 +0300

    feat: implement main-nav component

---
+++ src/widgets/app-header/_ui/main-nav.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+export function MainNav() {
+  return (
+    <nav className="flex items-start md:items-center gap-6 text-sm font-medium flex-col md:flex-row ">
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/home"
+      >
+        Home
+      </Link>
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/tours"
+      >
+        Author Tours
+      </Link>
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/youtube"
+      >
+        Youtube
+      </Link>
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/reports"
+      >
+        Reports
+      </Link>
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/blog"
+      >
+        Blog
+      </Link>
+      <Link
+        className="transition-colors hover:text-foreground/80 text-foreground/60"
+        href="/about"
+      >
+        About
+      </Link>
+    </nav>
+  )
+}
