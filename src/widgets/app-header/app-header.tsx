cd5b9d88b47e705c532d9bc9c906887b5e7ed594
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-12 09:35:47 +0300

    feat: implement logo header component

---
+++ src/widgets/app-header/app-header.tsx
@@ -0,0 +1,11 @@
+import { LogoIcon } from '@/shared/ui/logo-icon'
+import { Logo } from './_ui/logo'
+
+export function AppHeader() {
+  return (
+    <div>
+      AppHeader
+      <Logo />
+    </div>
+  )
+}
