89cad8a7491bdddefd9e0e9ed89bce9c1ea0359c
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-18 00:40:56 +0300

    chore: change markdown file type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,8 +2,8 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
-  filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  filePathPattern: `**/*.md`,
+  contentType: 'markdown',
   fields: {
     title: { type: 'string', required: true },
     description: { type: 'string', required: true },
