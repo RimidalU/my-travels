09d7092f00c6f2dc3e09fdcfc55170bc0c36653e
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-17 12:38:12 +0300

    feat: add contentlayer config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+// const { withContentlayer } = require('next-contentlayer')
+import { withContentlayer } from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default nextConfig
+export default withContentlayer(nextConfig)
