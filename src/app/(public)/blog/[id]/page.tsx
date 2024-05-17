1b0d746e2ae89f0bc9184ebb30bf13268ee4a359
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-17 12:41:57 +0300

    feat: add test posts page layout

--- src/app/(public)/blog/[id]/page.tsx
+++ src/app/(public)/blog/[id]/page.tsx
@@ -1,5 +1,27 @@
+import { allPosts } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+import { getMDXComponent } from 'next-contentlayer/hooks'
+import Link from 'next/link'
+
+function getPostBySlug(slug: string) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug)
+  return post ?? notFound()
+}
+
 export default function PostPage({ params }: { params: { id: string } }) {
+  const post = getPostBySlug(params.id)
+
+  const MDXContent = getMDXComponent(post.body.code)
   return (
-    <div className="container py-10 flex-1">PostPage --- {params.id} ---</div>
+    <div className="container py-10 flex-1">
+      <Link
+        href="/blog"
+        className="transition-colors hover:text-foreground/80 text-foreground/60 block mb-8"
+      >
+        ⬅ Back to Blog
+      </Link>
+
+      <MDXContent />
+    </div>
   )
 }
--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,3 +1,22 @@
+import { cn } from '@/shared/ui/utils'
+import { allPosts, Post } from 'contentlayer/generated'
+import Link from 'next/link'
+
 export default function BlogPage() {
-  return <div className="container py-10 flex-1">BlogPage</div>
+  return (
+    <div className="container py-10 flex-1">
+      <div className={cn('flex flex-col gap-3')}>
+        {allPosts.map((post: Post) => (
+          <Link
+            key={post._id}
+            href={post.url}
+            className="transition-colors hover:text-foreground/80 text-foreground/60"
+          >
+            {post.title}
+            <span className="sr-only">{`to ${post.slug} post`}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
