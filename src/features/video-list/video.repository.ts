dcdad0378e52c1199988a21cba128f767dc2ec74
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-18 23:36:55 +0300

    feat: implement video repository

---
+++ src/features/video-list/video.repository.ts
@@ -0,0 +1,33 @@
+import { dbClient } from '@/shared/lib/db'
+import { cache } from 'react'
+import {
+  CreateVideoListElementCommand,
+  RemoveVideoListElementCommand,
+  VideoListElement,
+} from './model/types'
+
+class VideoRepository {
+  getVideoList = cache(
+    (): Promise<VideoListElement[]> => dbClient.video.findMany(),
+  )
+
+  createVideoListItem = (
+    command: CreateVideoListElementCommand,
+  ): Promise<VideoListElement> => {
+    return dbClient.video.create({
+      data: command,
+    })
+  }
+
+  removeVideoListItem = (
+    command: RemoveVideoListElementCommand,
+  ): Promise<VideoListElement> => {
+    return dbClient.video.delete({
+      where: {
+        id: command.id,
+      },
+    })
+  }
+}
+
+export const videoRepository = new VideoRepository()
