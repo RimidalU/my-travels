44d08eff3999286aaee6468bbf1b077a25109f13
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-18 23:41:13 +0300

    fix: change video-list-element type fields

--- src/features/video-list/model/types.ts
+++ src/features/video-list/model/types.ts
@@ -3,21 +3,21 @@ import { EVENT_TYPE } from '@/shared/types/types'
 export type VideoListElement = {
   id: string
   title: string
-  description?: string
-  post?: string
+  description?: string | null
+  post?: string | null
   photo: string
   video: string
-  date?: Date
-  eventType: EVENT_TYPE
+  date: string
+  type: EVENT_TYPE
 }
 
 export type CreateVideoListElementCommand = {
   title: string
-  description?: string
-  post?: string
+  description?: string | null
+  post?: string | null
   photo: string
   video: string
-  date?: Date
+  date: string
   type: EVENT_TYPE
 }
 
