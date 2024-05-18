0952768df39550c6013f948ea96aa189affb9d35
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-18 20:47:03 +0300

    feat: add posts-list component to blog page

--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,21 +1,10 @@
-import { cn } from '@/shared/ui/utils'
-import { allPosts, Post } from 'contentlayer/generated'
-import Link from 'next/link'
+import { PostsList } from '@/features/posts-list/public/posts-list'
 
 export default function BlogPage() {
   return (
     <main className="container py-10 flex-1">
-      <div className={cn('flex flex-col gap-3')}>
-        {allPosts.map((post: Post) => (
-          <Link
-            key={post._id}
-            href={post.url}
-            className="transition-colors hover:text-blue-800 text-blue-600"
-          >
-            {post.title}
-            <span className="sr-only">{`to ${post.slug} post`}</span>
-          </Link>
-        ))}
+      <div className={'flex flex-col gap-3'}>
+        <PostsList className="" />
       </div>
     </main>
   )
